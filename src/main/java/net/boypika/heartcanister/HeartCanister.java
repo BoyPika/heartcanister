e0a95d981d17ae14d341eabb7647a0ac1ee91e74
BoyPika <boypika85@gmail.com>
2023-08-20 04:24:45 -0400

    Fix Errors Presented by the Config

--- src/main/java/net/boypika/heartcanister/HeartCanister.java
+++ src/main/java/net/boypika/heartcanister/HeartCanister.java
@@ -8,11 +8,11 @@ import net.boypika.heartcanister.potion.ModPotions;
 import net.boypika.heartcanister.trinkets.HeartCanisterItem;
 import net.boypika.heartcanister.trinkets.HeartItem;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.entity.effect.*;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
+import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
@@ -26,19 +26,19 @@ import top.offsetmonkey538.monkeyconfig538.ConfigManager;
 import static net.boypika.heartcanister.util.ModItemGroup.HEARTCANISTERGROUP;
 
 public class HeartCanister implements ModInitializer {
-    public static final Item JEWELED;
-    public static final Item NECROTIC;
-    public static final Item CANISTER;
-    public static final Item REDCANISTER;
-    public static final Item REDHEART;
-    public static final Item YELLOWCANISTER;
-    public static final Item YELLOWHEART;
-    public static final Item GREENCANISTER;
-    public static final Item GREENHEART;
+    public static Item JEWELED;
+    public static Item NECROTIC;
+    public static Item CANISTER;
+    public static Item REDCANISTER;
+    public static Item REDHEART;
+    public static Item YELLOWCANISTER;
+    public static Item YELLOWHEART;
+    public static Item GREENCANISTER;
+    public static Item GREENHEART;
     public static final InstantStatusEffect GREENHEARTEFFECT = registerEffect(Identifier("green_heart"), new GreenHeartEffect(StatusEffectCategory.BENEFICIAL, 56133));
     public static final InstantStatusEffect YELLOWHEARTEFFECT = registerEffect(Identifier("yellow_heart"), new YellowHeartEffect(StatusEffectCategory.BENEFICIAL, 16776451));
     public static final InstantStatusEffect REDHEARTEFFECT = registerEffect(Identifier("red_heart"), new RedHeartEffect(StatusEffectCategory.BENEFICIAL, 16318464));
-    static {
+    public static void registerItems() {
         JEWELED = registerItem("jeweled_apple", 64, new FoodComponent.Builder().hunger(4).saturationModifier(1.2F).statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 1200, 0), 1).statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 1200, 0), 1).statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 4),1).statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 1200,0),1).alwaysEdible().build());
         NECROTIC = registerItem("necrotic_bone",64, null);
         CANISTER = registerItem("empty_canister", config().nestedStackCount.CanisterStackCount, null);
@@ -75,9 +75,9 @@ public class HeartCanister implements ModInitializer {
     private static final Identifier FORTRESS
             = new Identifier("minecraft", "chests/nether_bridge");
     public static void modifyLootTables(){
-        LootTableLoadingCallback.EVENT.register(((resourceManager, lootManager, id, tableBuilder, source) -> {
+        LootTableEvents.MODIFY.register(((resourceManager, lootManager, id, tableBuilder, source) -> {
             if (DUNGEON.equals(id)){
-                FabricLootPoolBuilder pool = FabricLootPoolBuilder.builder()
+                LootPool.Builder pool = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(config().nestedLootChance.RedHeartLootChance))
                         .with(ItemEntry.builder(REDHEART))
@@ -85,7 +85,7 @@ public class HeartCanister implements ModInitializer {
                 tableBuilder.pool(pool);
             }
             if (DUNGEON.equals(id)){
-                FabricLootPoolBuilder pool = FabricLootPoolBuilder.builder()
+                LootPool.Builder pool = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(config().nestedLootChance.YellowHeartDungeonLootChance))
                         .with(ItemEntry.builder(YELLOWHEART))
@@ -93,7 +93,7 @@ public class HeartCanister implements ModInitializer {
                 tableBuilder.pool(pool);
             }
             if (FORTRESS.equals(id)){
-                FabricLootPoolBuilder pool = FabricLootPoolBuilder.builder()
+                LootPool.Builder pool = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(config().nestedLootChance.YellowHeartFortressLootChance))
                         .with(ItemEntry.builder(YELLOWHEART))
@@ -101,7 +101,7 @@ public class HeartCanister implements ModInitializer {
                 tableBuilder.pool(pool);
             }
             if (FORTRESS.equals(id)){
-                FabricLootPoolBuilder pool = FabricLootPoolBuilder.builder()
+                LootPool.Builder pool = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(config().nestedLootChance.GreenHeartLootChance))
                         .with(ItemEntry.builder(GREENHEART))
@@ -112,6 +112,8 @@ public class HeartCanister implements ModInitializer {
     }
     @Override
     public void onInitialize() {
+        ConfigManager.init(new MyConfigModel(), "heartcanister");
+        registerItems();
         modifyLootTables();
         ModPotions.registerPotionRecipes();
     }
